2287d247835f5f8b12e336f3e7c053530518f9f0
Gar <gar@lonelydatu.com>
2021-01-07 15:05:44 -0500

    ON

---
+++ dev/social-a_540x675/_js/main.js
@@ -0,0 +1,47 @@
+import {size, banners, end, init_set} from '../../_common/js/common.js'
+
+const tl = new TimelineMax()
+
+
+function start(){
+	
+	
+	
+	init_set()
+	
+	
+	
+	
+	
+
+	tl.from(".t1a", .4, {opacity:0}, "+=.2")
+	tl.from(".t1b", .4, {opacity:0}, "+=.2")
+	TweenMax.from(".line", 1, {clip: `rect(0px, ${0}px, ${size.hh}px, 0px)`}, 2)		
+	tl.from(".t1c", .4, {opacity:0}, "+=.2")
+	
+
+	tl.to(".person", 1, {scale:.5, ease:Power3.easeOut} )
+
+	tl.from(".terms", 1, {opacity:0}, "+=.2")
+	
+	
+	
+	
+
+	
+	
+	
+	// tl.gotoAndPlay("end")
+}
+
+
+
+
+
+
+
+start()
+
+
+module.exports = {};
+
---
+++ dev/social-c_540x675/_js/main.js
@@ -0,0 +1,45 @@
+import {size, banners, end, init_set} from '../../_common/js/common.js'
+
+const tl = new TimelineMax()
+const read = banners.opt1a
+
+function start(){
+	
+	
+	
+	init_set()
+	
+	
+	
+	
+	
+
+	tl.from(".t1a", .4, {opacity:0}, "+=.2")
+	tl.from(".t1b", .4, {opacity:0}, "+=.2")
+	TweenMax.from(".line", 1, {clip: `rect(0px, ${0}px, ${size.hh}px, 0px)`}, 2)		
+	tl.from(".t1c", .4, {opacity:0}, "+=.2")
+	tl.from(".terms", .4, {opacity:0}, "+=.2")
+
+	tl.set("#logo_hwc_1", {className:"rotating"}, 2)		
+	
+	
+	
+	
+
+	
+	
+	
+	// tl.gotoAndPlay("end")
+}
+
+
+
+
+
+
+
+start()
+
+
+module.exports = {};
+
