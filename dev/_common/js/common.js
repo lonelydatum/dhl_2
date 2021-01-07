f899f050835cb6135373f0db36293ea53385d775
Gar <gar@lonelydatu.com>
2021-01-06 22:03:46 -0500

    on

--- dev/_common/js/common.js
+++ dev/_common/js/common.js
@@ -3,4 +3,35 @@ const size = {ww:2*banner.offsetWidth, w:banner.offsetWidth, h:banner.offsetHeig
 
 TweenLite.defaultEase = Power2.easeInOut
 
-export {size}
+
+
+function init_set() {
+	TweenMax.set([".line"], {clip: `rect(0px, ${size.ww}px, ${size.hh}px, 0px)`})	
+	TweenMax.set(".frame1", {opacity:1})
+	TweenMax.set(".person",  {scale:0, x:-size.w, y:-size.h, transformOrigin:"100% 100%"})
+}
+
+function end(read){
+	const tl_end = new TimelineMax()
+	tl_end.add("end", `+=${read.t2}`)
+	tl_end.set(".logo_hwc_1", {className:"rotating"}, "end")	
+	tl_end.to(".frame1", .3, {opacity:0}, "end")
+	tl_end.to(".frame2", .3, {opacity:1}, "+=.2")
+	tl_end.from([".t3_terms", ".t3"], .7, {opacity:0}, "+=.5")
+	return tl_end
+}
+
+const banners = {
+	opt1a:{
+		t1: 3.7,
+		t2: 3.7
+	},
+	opt1b:{
+		t1: 2.5,
+		t2: 3
+	}
+}
+
+
+
+export {size, banners, end, init_set}
