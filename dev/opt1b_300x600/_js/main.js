48b03a02b353ace81ccf40a06c603211e7d0e33f
Gar <gar@lonelydatu.com>
2021-01-06 22:07:11 -0500

    on

--- dev/opt1b_300x600/_js/main.js
+++ dev/opt1b_300x600/_js/main.js
@@ -36,8 +36,8 @@ function start(){
 function grow(){
 	const tl_bang = new TimelineMax()
 	tl_bang.add("bang", 1)
-	tl_bang.from(".line_1", 1.6, {clip: `rect(0px, ${0}px, ${size.hh}px, 0px)`}, "bang")		
-	tl_bang.to(".person", 1.2, {scale:.5, ease:Power3.easeOut}, "bang")
+	tl_bang.from(".line_1", 1, {clip: `rect(0px, ${0}px, ${size.hh}px, 0px)`}, "bang")		
+	tl_bang.to(".person", .7, {scale:.5, ease:Power4.easeOut}, "bang")
 }
 
 
