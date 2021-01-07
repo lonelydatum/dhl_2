a09c6eb898d84a09f3daeaab4299414f97d78f7e
Gar <gar@lonelydatu.com>
2021-01-06 22:43:03 -0500

    on

---
+++ dev/opt1b_160x600/_js/main.js
@@ -0,0 +1,50 @@
+import {size, banners, end, init_set} from '../../_common/js/common.js'
+
+const tl = new TimelineMax()
+const read = banners.opt1a
+
+function start(){
+	
+	
+	
+	init_set()
+	TweenMax.set(".person",  {scale:0, x:-size.w-44, y:-size.h, transformOrigin:"100% 100%"})
+	
+	grow()
+
+	
+	
+	
+	
+
+	tl.from(".t1", .4, {x:-size.w}, "+=.2")
+	
+	
+	
+	tl.add("f2", `+=${read.t1}`)
+	tl.to([".line", ".t1"], .5, {opacity:0, x:size.w}, "f2")
+	tl.from(".t2", .5, {x:-size.w}, "f2")
+
+	tl.add( end(read) )
+	
+	
+	// tl.gotoAndPlay(10)
+}
+
+
+
+
+
+function grow(){
+	const tl_bang = new TimelineMax()
+	tl_bang.add("bang", 1)
+	tl_bang.from(".line", 1, {clip: `rect(0px, ${0}px, ${size.hh}px, 0px)`}, "bang")		
+	tl_bang.to(".person", .7, {scale:.5, ease:Power3.easeOut}, "bang")
+}
+
+
+start()
+
+
+module.exports = {};
+
