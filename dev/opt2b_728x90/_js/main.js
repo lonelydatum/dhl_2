8916dc898d3a61b08ace0b77954c4111d686f040
Gar <gar@lonelydatu.com>
2021-01-13 15:57:52 -0500

    ON

---
+++ dev/opt2b_728x90/_js/main.js
@@ -0,0 +1,41 @@
+import {size, banners, end, init_set} from '../../_common/js/common.js'
+
+const tl = new TimelineMax()
+const read = banners.opt1b
+
+function start(){
+	
+	
+	
+	init_set()
+	
+
+	
+	
+	
+
+	tl.from(".t1", .4, {x:-size.w}, "+=.2")
+	// tl.from(".line_1", 1, {clip: `rect(0px, ${0}px, ${size.hh}px, 0px)`})		
+	
+	
+	
+	tl.add("f2", read.t1)
+	tl.to([".t1"], .5, {opacity:0, x:size.w}, "f2")
+	tl.from(".t2", .5, {x:-size.w}, "f2")
+	// tl.from(".line_2", 1, {clip: `rect(0px, ${0}px, ${size.hh}px, 0px)`})		
+	tl.add( end(read) )
+	
+	
+	// tl.gotoAndPlay("end")
+}
+
+
+
+
+
+
+start()
+
+
+module.exports = {};
+
