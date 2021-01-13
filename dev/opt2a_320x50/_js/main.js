e852b9834c9b6611b1b602fb3295ab633a65815a
Gar <gar@lonelydatu.com>
2021-01-13 15:17:59 -0500

    ON

--- dev/opt2a_320x50/_js/main.js
+++ dev/opt2a_320x50/_js/main.js
@@ -20,6 +20,7 @@ function start(){
 	
 	
 	tl.add("f2", read.t1)
+	tl.to([".t1"], .5, {opacity:0, x:size.w}, "f2")
 	// tl.to([".line", ".t1"], .5, {opacity:0, x:size.w}, "f2")
 	tl.from(".t2", .5, {x:-size.w}, "f2")
 
